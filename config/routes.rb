9eb79649cb13b49460efc5a268679ea33178fc40
Ryan Holm <ryan.j.holm@gmail.com>
2014-06-20 06:14:23 +0000

    Associated and nested topics and posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Bloccit::Application.routes.draw do
   
   devise_for :users
+
+  resources :topics do
+    resources :posts, except: [:index]
+  end
+
   resources :posts
 
   get 'about' => 'welcome#about'
