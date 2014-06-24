4a7b29e9c3a8487aa1b018488e37289afb8f5ecb
Ryan Holm <ryan.j.holm@gmail.com>
2014-06-24 04:17:02 +0000

    Added image to Post 2.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140623042133) do
+ActiveRecord::Schema.define(version: 20140624034709) do
 
   create_table "comments", force: true do |t|
     t.text     "body"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20140623042133) do
     t.datetime "updated_at"
     t.string   "role"
     t.string   "avatar"
+    t.string   "image"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
