213e919aad4f1010665e9dc0821e5d5cb39fea07
Ryan Holm <ryan.j.holm@gmail.com>
2014-07-05 16:23:56 -0600

    updated post controller again last push didnt work.

--- app/controllers/topics/posts_controller.rb
+++ app/controllers/topics/posts_controller.rb
@@ -1,10 +1,8 @@
 class Topics::PostsController < ApplicationController
-  before_filter :authenticate_user!
-
   def show
     @topic = Topic.find(params[:topic_id])
-    authorize @topic
     @post = Post.find(params[:id])
+    authorize @topic
     @comments = @post.comments
     @comment = Comment.new
   end
@@ -25,10 +23,10 @@ class Topics::PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = current_user.posts.build(post_params)
     @post.topic = @topic
+
     authorize @post
     if @post.save
-      flash[notice:] = "Post was saved successfully."
-      redirect_to [@topic, @post]
+      redirect_to [@topic, @post], notice: "Post was saved successfully."
     else
       flash[:error] = "There was an error saving the post. Please try again."
       render :new
@@ -44,16 +42,17 @@ class Topics::PostsController < ApplicationController
       redirect_to [@topic, @post]
     else
       flash[:error] = "There was an error saving the post. Please try again."
-      render :edit
+      render :new
     end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :image)
   end
 
   def destroy
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
-    authorize @post
-    @comments = @post.comments
-    @comment = Comment.new
 
     title = @post.title
     authorize @post
@@ -64,11 +63,5 @@ class Topics::PostsController < ApplicationController
       flash[:error] = "There was an error deleting the post."
       render :show
     end
-  end
-
-  private
-
-  def post_params
-    params.require(:post).permit(:title, :body, :topic_id, :image)
   end
 end
