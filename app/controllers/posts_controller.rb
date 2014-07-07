4f41fac61a6b0101c9d73473f7ca572e75bedc92
Ryan Holm <ryan.j.holm@gmail.com>
2014-07-06 22:35:10 -0600

    added authorize post in index.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.visible_to(current_user).where("posts.created_at > ?",7.days.ago).paginate(page: params[:page], per_page: 10)
+    authorize @posts 
   end
 
   def show
