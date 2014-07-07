268f1091f179804c0b64c4e3d42f9606f684c933
Ryan Holm <ryan.j.holm@gmail.com>
2014-07-06 22:41:26 -0600

    rearranged reources favorite.

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,10 @@ Bloccit::Application.routes.draw do
   
   resources :topics do
     resources :posts, except: [:index], controller: 'topics/posts' do
-      resources :comments, only: [:create, :destroy]
-        resources :favorites, only: [:create, :destroy]
+      resources :comments, only: [:create, :destroy]  
       get '/up-vote' => 'votes#up_vote', as: :up_vote
       get '/down-vote' => 'votes#down_vote', as: :down_vote
+      resources :favorites, only: [:create, :destroy]
     end
   end
 
