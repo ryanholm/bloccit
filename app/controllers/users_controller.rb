ce9a6c07e8c3b3aeb92cbcf7c2b85637c04e6c6b
Ryan Holm <ryan.j.holm@gmail.com>
2014-07-06 23:55:47 -0600

    deleted the wrong file hopefully fixes.

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+   def update
+     if current_user.update_attributes(user_params)
+       flash[:notice] = "User information updated"
+       redirect_to edit_user_registration_path(current_user)
+     else
+       render "devise/registrations/edit"
+     end
+   end
+ 
+   def show
+     @user = User.find(params[:id])
+     @posts = @user.posts.visible_to(current_user)
+   end
+ 
+   def index
+     @users = User.top_rated.paginate(page: params[:page], per_page: 10)
+   end
+ 
+   private
+ 
+   def user_params
+     params.require(:user).permit(:name, :avatar, :email_favorites)
+   end
+ end
